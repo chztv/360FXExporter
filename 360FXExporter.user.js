3c28b69cf08e7b07ebe0e75e3175d9f5030adaaf
chztv <0571chz@gmail.com>
2013-09-04 13:51:40 +0800

    jqsc add

--- 360FXExporter.user.js
+++ 360FXExporter.user.js
@@ -14,4 +14,5 @@ jqscript.src = "http://libs.baidu.com/jquery/1.8.3/jquery.min.js";
 var script = document.createElement('script');
 script.id = "TLE_script";
 script.src = "https://raw.github.com/chztv/360FXExporter/master/360FXExporter.js";
+document.body.appendChild(jqscript);
 document.body.appendChild(script);
