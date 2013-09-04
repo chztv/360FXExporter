9941188128bb52280d56ab38303a82c0fb95079f
chztv <0571chz@gmail.com>
2013-09-04 13:49:33 +0800

    add jquery

--- 360FXExporter.user.js
+++ 360FXExporter.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name       360FXExporter
 // @namespace  http://yunpan.cn/
-// @version    0.1
+// @version    0.2
 // @description  export 360 share url to aria2/wget
 // @match      http://*.yunpan.cn/*
 // @run-at document-end
 // @copyright  2013+, chztv <0571chz@gmail.com>
 // ==/UserScript==
 
+var jqscript = document.createElement('jqscript');
+jqscript.id = "jq_script";
+jqscript.src = "http://libs.baidu.com/jquery/1.8.3/jquery.min.js";
 var script = document.createElement('script');
 script.id = "TLE_script";
 script.src = "https://raw.github.com/chztv/360FXExporter/master/360FXExporter.js";
