8a136247a4372126c89a7a28e073fcbe213d033e
chztv <0571chz@gmail.com>
2014-03-19 23:18:13 +0800

    Update 360FXExporter.js

--- 360FXExporter.js
+++ 360FXExporter.js
@@ -282,6 +282,7 @@ if (SYS_CONF.isSingle) {
 			dataType: "json",
 			success:function(data){
                 		alert('this ok');
+                		alert(W(".word-dl").html());
                 		$(".qrcode-body").html('<img id="qrcode-img" src="http://c3.yunpan.360.cn/share/getDLinkQRcode?fullurl=http%3A%2F%2Fajnlsff3wa.l3.yunpan.cn%2Flk%2FQXig3WVxUZjW9"><p>链接可复制，可Aria2</p><a id="aria2url1" href="'+data.data.downloadurl+'" target="_blank" title="可复制下载地址">直接下载</a> | <a id="aria2down1" onclick="aria2down1();">Aria2</a> | <a id="setting" onclick="settingshow();">设置</a>');
                 		$("#dumpTip").html(' | <a id="aria2down1" onclick="aria2down1();">Aria2</a> | <a id="setting" onclick="settingshow();">设置</a>');
 			 },
